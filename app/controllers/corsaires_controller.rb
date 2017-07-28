387f5699cafbca03d6c0b660c0304ff2a2968b70
Juan Pablo ANGARITA <juanpabloangarita@gmail.com>
2017-07-28 13:11:51 +0200

    my home is ready, thanks christelle ;)

---
+++ app/controllers/corsaires_controller.rb
@@ -0,0 +1,13 @@
+class CorsairesController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  get '/new', to: 'corsaires#new'
+
+  get '/show', to: 'corsaires#show'
+
+  get '/edit', to: 'corsaires#edit'
+
+  get 'corsaires', to: 'corsaires#index'
+
 end
