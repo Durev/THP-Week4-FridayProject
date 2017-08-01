ac9b5efc245df07b5763157fa09a6368e3878790
Juan Pablo ANGARITA <juanpabloangarita@gmail.com>
2017-08-01 10:52:45 +0200

    crud finished

--- app/controllers/corsaires_controller.rb
+++ app/controllers/corsaires_controller.rb
@@ -14,9 +14,26 @@ class CorsairesController < ApplicationController
   end
 
   def edit
+    @corsaire = Corsaire.find(params[:id])
+    #redirect_to corsaire_path(@corsaire.id)
+    #esto no funciona, ya que inmediatamente me lleva a corsaire_path osea, show, y nunca llego a edit-nosense
+  end
+
+  def update
+    @corsaire = Corsaire.find(params[:id])
+    @corsaire.update(corsaire_params)
+    redirect_to corsaires_path(@corsaire.id) #redirecting to INDEX (grafikart) otherwise to show->corsaire_path
+
   end
 
   def index
+    @corsaire = Corsaire.all #creo que este es el que le envía a  la página index la info
+  end
+
+  def destroy
+    @corsaire = Corsaire.find(params[:id])
+    @corsaire.destroy
+    redirect_to corsaires_path(@corsaire.id) #redirect to index
   end
 
   def corsaire_params
